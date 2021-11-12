4acaf4f7e969e86a43ff4bf68dbc856e084dac68
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-11-12 16:52:16 -0300

    Ajustes DIP

--- src/main/java/br/com/fascari/solid/dip/payment/Payment.java
+++ src/main/java/br/com/fascari/solid/dip/payment/Payment.java
@@ -6,6 +6,11 @@ import br.com.fascari.solid.dip.model.IDbProduct;
 
 public class Payment {
     public void pay(String productID) {
+        /*
+        * Neste cenário utiliza-se o padrão factory method para abstrair o tipo do banco a ser utilizado
+        * e permitir o desacoplamento de baixo nível dos bancos de dados, utilizado uma interface
+        * como abstração.
+        * */
         //IDbProduct dbProduct = DbProductFactory.create(Db.MYSQL);
         IDbProduct dbProduct = DbProductFactory.create(Db.MONGODB);
         String product = dbProduct.getProductById(productID);
