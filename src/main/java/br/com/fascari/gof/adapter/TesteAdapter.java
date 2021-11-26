66f0909d91877f50155a29893b768a7291595389
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-11-26 15:46:38 -0300

    GOF: Adapter

--- src/main/java/br/com/fascari/gof/adapter/TesteAdapter.java
+++ src/main/java/br/com/fascari/gof/adapter/TesteAdapter.java
@@ -1,15 +1,18 @@
 package br.com.fascari.gof.adapter;
 
 import br.com.fascari.gof.adapter.adapters.PayoneerAdapter;
+import br.com.fascari.gof.adapter.payoneer.IPayonnerPayments;
 import br.com.fascari.gof.adapter.payoneer.Payoneer;
 import br.com.fascari.gof.adapter.paypal.IPayPalPayments;
 
 public class TesteAdapter {
 
     public static void main(String[] args) {
-        //IPayonnerPayments payment = new Payoneer();
-        IPayPalPayments payment = new PayoneerAdapter(new Payoneer());
+        IPayonnerPayments payonnerPayments = new Payoneer();
+        payonnerPayments.sendPayment();
+        payonnerPayments.receivePayment();
 
+        IPayPalPayments payment = new PayoneerAdapter(new Payoneer());
         payment.paypalPayment();
         payment.paypalReceive();
     }
